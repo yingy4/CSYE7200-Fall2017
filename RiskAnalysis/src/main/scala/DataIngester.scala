345fec77a17dfee508e1c99520a1de7a3c19a570
Sarang Wardadkar <wardadkar.s@husky.neu.edu>
2017-11-19 12:11:22 -0500

    Got basic Spark RDD running

--- RiskAnalysis/src/main/scala/DataIngester.scala
+++ RiskAnalysis/src/main/scala/DataIngester.scala
@@ -1,3 +1,16 @@
-class DataIngester {
+import org.apache.spark.{SparkConf, SparkContext}
 
+class DataIngester(sc: SparkContext) {
+  val rdd = sc.textFile("train.csv").toJavaRDD()
+  val result = rdd.count()
+  println(result)
+}
+
+
+object DataIngester {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("RiskAnalysis").setMaster("local")
+    val sc = new SparkContext(conf)
+    val Ingester = new DataIngester(sc)
+  }
 }
