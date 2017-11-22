53acb933489a9e36da871898f27669bfddf290d9
Sarang Wardadkar <wardadkar.s@husky.neu.edu>
2017-11-22 18:55:56 -0500

    Adding Linear Regression POC

--- RiskAnalysis/src/main/scala/RiskAnalysisApplication.scala
+++ RiskAnalysis/src/main/scala/RiskAnalysisApplication.scala
@@ -1,7 +1,12 @@
+import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.ml.feature._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit, TrainValidationSplitModel}
+import org.apache.spark.sql.functions._
 
 class RiskAnalysisApplication(sparkSession: SparkSession) {
   val data: DataFrame = sparkSession.read
@@ -10,10 +15,6 @@ class RiskAnalysisApplication(sparkSession: SparkSession) {
                   .format("csv")
                   .load("IntermediateData.csv")
   data.printSchema()
-
-  //val df = (data.select(data("response").as("label")))
-
-  //df.show()
 
   val colNames: Array[String] = Array("Product_Info_4", "Ins_Age", "BMI", "Employment_Info_1",
     "Employment_Info_4", "Employment_Info_6", "Insurance_History_5", "Family_Hist_2",
@@ -24,14 +25,14 @@ class RiskAnalysisApplication(sparkSession: SparkSession) {
   // Use VectorAssembler to convert the input columns of the cancer data
   // to a single output column of an array called "features"
   // Set the input columns from which we are supposed to read the values.
-  val assembler: VectorAssembler = new VectorAssembler().setInputCols(colNames).setOutputCol("features")
+  val assembler: VectorAssembler = new VectorAssembler().setInputCols(colNames).setOutputCol("featuresIntermediate")
 
   // Use the assembler to transform our DataFrame to a single column: features
-  val output: DataFrame = assembler.transform(data).select("features")
+  val output: DataFrame = assembler.transform(data).select("featuresIntermediate")
 
   // Use StandardScaler on the data
   val scaler: StandardScaler = new StandardScaler()
-    .setInputCol("features")
+    .setInputCol("featuresIntermediate")
     .setOutputCol("scaledFeatures")
     .setWithStd(true)
     .setWithMean(false)
@@ -47,7 +48,7 @@ class RiskAnalysisApplication(sparkSession: SparkSession) {
   // Then fit this to the scaledData
   val pca: PCAModel = new PCA()
     .setInputCol("scaledFeatures")
-    .setOutputCol("pcaFeatures")
+    .setOutputCol("features")
     .setK(4)
     .fit(scaledData)
 
@@ -56,23 +57,39 @@ class RiskAnalysisApplication(sparkSession: SparkSession) {
   val pcaDF: DataFrame = pca.transform(scaledData)
 
   // Show the new pcaFeatures
-  val result: DataFrame = pcaDF.select("pcaFeatures")
-  result.show()
+  val result: DataFrame = pcaDF.select("features")
 
-//  result.write
-//        .format("com.databricks.spark.csv")
-//        .option("header", "true")
-//        .save("IntermediateTestdata.csv")
+  /*
+   * In order to join these two data frames we need to add a unique index to each of these DF's
+   */
+  val dataWithUniqueId: DataFrame = data.withColumn("row_id_1", monotonically_increasing_id())
+  val resultWithUniqueId: DataFrame = result.withColumn("row_id_2", monotonically_increasing_id())
 
-
-
-
-  //val output2 = assembler.transform(df).select("label","pcaFeatures")
-
-  //output2.show()
+  // this DF contains the response column
+  val finalDF: DataFrame = dataWithUniqueId.as("df1").join(resultWithUniqueId.as("df2"), dataWithUniqueId("row_id_1") === resultWithUniqueId("row_id_2"), "inner")
+                           .select("df2.features", "df1.response")
 
 
+  val Array(training,test) = finalDF.randomSplit(Array(0.70, 0.30))
 
+  val lr = new LinearRegression()
+
+  val paramGrid: Array[ParamMap] = new ParamGridBuilder().build()
+
+  test.show(4, false)
+
+  val trainValidationSplit: TrainValidationSplit = new TrainValidationSplit()
+    .setEstimator(lr)
+    .setEvaluator(new RegressionEvaluator())
+    .setEstimatorParamMaps(paramGrid)
+    .setTrainRatio(0.8)
+
+  val model: TrainValidationSplitModel = trainValidationSplit.fit(training)
+
+  // TODO: Rename Response column to label and this should start working
+  model.transform(test).select("features", "response", "prediction").show()
+
+  model.validationMetrics
 }
 
 object RiskAnalysisApplication {
