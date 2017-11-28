2057bd5176116a32023a1479855723720045c364
mickymount <m.tiburu@gmail.com>
2017-11-28 18:52:24 -0500

    Final working cleaning data ready

--- RiskAnalysis/src/main/scala/DataIngester.scala
+++ RiskAnalysis/src/main/scala/DataIngester.scala
@@ -1,72 +1,31 @@
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.functions.{sum, col, when, count, isnan}
-import org.apache.spark.sql.functions.udf
-import org.apache.log4j._
-import org.apache.spark.rdd.RDD
-
-import scala.util.{Success, Try}
-
-
-//class DataIngester(sc: SparkContext) {
-//  val rdd = sc.textFile("train.csv").toJavaRDD()
-//  val result = rdd.count()
-//  println(result)
-//}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.functions._
+import java.util.Calendar
+import org.apache.log4j.{Level, Logger}
+//import org.apache.spark.ml.dmlc.xgboost4j.scala.spark.XGBoost
 
 
 object DataIngester {
 
-  def main(args: Array[String]) {
-    def countNulls = {udf((v: Any) => if (v == null || v == "") 1 else 0)}
+  def run(sparkSession: SparkSession){
 
-    //    val spark = SparkSession.builder().appName("Risk Analysis").master("local[*]").getOrCreate()
-    //    // Set the log level to only print errors
-    //
-    //    Logger.getLogger("org").setLevel(Level.ERROR)
-    //
-    //    val pathFile = "train.csv"
-    //
-    //    val header: Row = spark.read.csv(pathFile).head()
-    //    val data: RDD[String] = spark.read.option("header", true).csv(pathFile)
-    //      .rdd.map(_.toString().replace("]","").replace("[","")).map(r => {
-    //      val tmp: String = r.split(",").take(3).mkString(",")
-    ////      r.split(",").foreach(println(_))
-    //      val remain: String = r.split(",").drop(3).filter(
-    //        x => {
-    //          Try(x.toDouble) match {
-    //        case Success(x) =>  x*0.6 != 0
-    //
-    //        case _ => false
-    //      }
-    //        }).mkString(",")
-    //      tmp + "," + remain
-    //    }
-    //    )
-    //    import spark.implicits._
-    ////    val headerString = spark.createDataset(Array(header.toString()))
-    //    // headerString.rdd.union(data).repartition(1).saveAsTextFile("testout")
-    //    data.saveAsTextFile("testout")
+    val df: DataFrame = sparkSession.read.option("header","true").option("inferSchema","true")
+      .option("delimeter","\t").csv("train.csv")
 
-    val spark = SparkSession.builder().master("local[*]").getOrCreate()
-    val df = spark.read.option("header","true").option("inferSchema","true").csv("/Users/mtiburu/Downloads/try.csv")
-    //    val newValue = df.na.replace(df.columns, Map("" -> "0"))
-    //    df.select(df.columns.map(c => sum(col(c).isNull.cast("int" +
-    //      "")).alias(c)): _*).show
-    //    var countCol: org.apache.spark.sql.Column = null
-    //    df.columns.foreach(c => {
-    //      if (countCol == null || countNulls == "") countCol = countNulls(col(c))
-    //      else countCol = countCol + countNulls(col(c))
-    //    });
-    //
-    //    println(countCol)
+    val newDf: DataFrame = df.drop("Product_Info_2")
 
 
-    val isNaN = udf((value : Float) => {
-      if (value.equals(Float.NaN) || value == null) true else false })
+    val null_thresh = df.count()*0.8
 
-    val result = df.filter(isNaN(df("Product_Info_3"))).count()
-    println(result)
-    df.write.format("csv").option("header","true").save("output")
+    val to_keep = newDf.columns.filter(c => newDf.agg(
+      sum(when(newDf(c) === 0 || newDf(c).isNull, 1).otherwise(0)).alias(c)).first().getLong(0) <= null_thresh
+    )
+
+    val clean = df.select(to_keep.head, to_keep.tail: _*)
+
+    val finalDf= clean.na.fill(clean.columns.zip(clean.select(clean.columns.map(mean(_)): _*).first.toSeq).toMap)
+
+    finalDf.coalesce(1).write.format("com.databricks.spark.csv").option("header", true).save("Trimed")
 
 
 
