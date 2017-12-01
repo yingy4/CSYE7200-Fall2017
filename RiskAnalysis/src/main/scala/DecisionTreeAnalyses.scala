f8f7d6cddee0337814376d666b7e95bdd37223f8
Harsh Pandey <Pandey.ha@husky.neu.edu>
2017-12-01 14:25:24 -0500

    Decision tree

--- RiskAnalysis/src/main/scala/DecisionTreeAnalyses.scala
+++ RiskAnalysis/src/main/scala/DecisionTreeAnalyses.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.sql.DataFrame
 
 class DecisionTreeAnalyses(dataFrame: DataFrame) {
@@ -14,6 +15,27 @@ class DecisionTreeAnalyses(dataFrame: DataFrame) {
     val predictions = model.transform(testData)
 
     predictions.show
+/*
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val impurity = "variance"
+    val maxDepth = 5
+    val maxBins = 32
+
+    val model = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo, impurity,
+      maxDepth, maxBins)
+
+    // Evaluate model on test instances and compute test error
+    val labelsAndPredictions = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testMSE = labelsAndPredictions.map{ case (v, p) => math.pow(v - p, 2) }.mean()
+    println("Test Mean Squared Error = " + testMSE)
+    println("Learned regression tree model:\n" + model.toDebugString)
+
+*/
+
+
   }
 
 }
