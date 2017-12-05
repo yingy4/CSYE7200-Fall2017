be98c3a8cdbadf66db5ea21dacf993fa517704c8
mickymount <m.tiburu@gmail.com>
2017-12-05 18:11:19 -0500

    including somemore unit test code

--- RiskAnalysis/src/main/scala/DataIngester.scala
+++ RiskAnalysis/src/main/scala/DataIngester.scala
@@ -40,9 +40,9 @@ class DataIngester {
     df.columns.contains(colName)
   }
 
-  def checkColumns(df:DataFrame): Boolean ={
+  def checkColumns(df:DataFrame): Boolean = {
 
-    df.filter(x=>)
+    df.columns.isEmpty
 
   }
 }
