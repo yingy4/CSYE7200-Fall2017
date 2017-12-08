77586153d5d1c114531a384b7d990a2ddaa0c177
mickymount <m.tiburu@gmail.com>
2017-12-08 17:48:43 -0500

    modified logistic linear regression

--- RiskAnalysis/src/main/scala/LogisticRegressionAnalysis.scala
+++ RiskAnalysis/src/main/scala/LogisticRegressionAnalysis.scala
@@ -1,5 +1,6 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, RegressionEvaluator}
 import org.apache.spark.sql.DataFrame
 
 class LogisticRegressionAnalysis(dataFrame:DataFrame) {
@@ -7,18 +8,34 @@ class LogisticRegressionAnalysis(dataFrame:DataFrame) {
  def run():Unit = {
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.ERROR)
-  val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
 
-  val lrModel = lr.fit(dataFrame)
+   val splits = dataFrame.randomSplit(Array(0.7, 0.3), seed = 11L)
+   val trainging = splits(0).cache()
+   val test = splits(1)
 
-  println(s"Coefficients: ${lrModel.coefficientMatrix} Intercept: ${lrModel.interceptVector}")
+   val binaryClassificationEvaluator = new BinaryClassificationEvaluator().setLabelCol("label")
+     .setRawPredictionCol("rawPrediction")
+   binaryClassificationEvaluator.setMetricName("areaUnderROC")
+   val regressionEvaluator = new RegressionEvaluator().setLabelCol("label").setPredictionCol("prediction")
+   regressionEvaluator.setMetricName("rmse")
 
-  val mlr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
-    .setFamily("multinomial")
+//   logistic Regression
+   val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+     .setFeaturesCol("features").setLabelCol("label")
+   val lrModel = lr.fit(trainging)
+   val predictions = lrModel.transform(trainging)
+   val areaTraining = binaryClassificationEvaluator.evaluate(predictions)
 
-  val mlrModel = mlr.fit(dataFrame)
-  println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}" +
-    s"Multinomial intercepts: ${mlrModel.interceptVector}")
+   println("Area under ROC using Logistic Regression on training data = " + areaTraining)
+
+   val predictionTest = lrModel.transform(test)
+   val areaTest = binaryClassificationEvaluator.evaluate(predictionTest)
+   println("Area under ROC using Logistic Regression on test data = " + areaTest)
+
+   val rmseLR = regressionEvaluator.evaluate(predictionTest)
+
+   println("Root Mean Squared Error (RMSE) Logistic Regression on test data = " + rmseLR)
+
 
  }
 }
